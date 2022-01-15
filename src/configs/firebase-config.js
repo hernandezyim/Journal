7d7f4876b8dac6a4a3c8879235669fe203d36380
Yim <yimhernandez1@hotmail.com>
2022-01-15 05:46:43 -0400

    update config

--- src/configs/firebase-config.js
+++ src/configs/firebase-config.js
@@ -3,18 +3,16 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5a7_cZyqs3r33w_XKMv4UZtyGwCzSK4E",
-  authDomain: "react-app-82896.firebaseapp.com",
-  projectId: "react-app-82896",
-  storageBucket: "react-app-82896.appspot.com",
-  messagingSenderId: "210680262150",
-  appId: "1:210680262150:web:7a93b47ff05e157bea9d87",
-  measurementId: "G-YCBBMCMHR9",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-const googleAuthProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleAuthProvider = new GoogleAuthProvider();
 
-export { db, auth, googleAuthProvider };
