16eb275b78d23c6b1a793a4c4e87ee7e018d55f0
Yim <yimhernandez1@hotmail.com>
2022-01-15 11:23:37 -0400

    update config

--- src/configs/firebaseConfig.js
+++ src/configs/firebaseConfig.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAjQ7t2M0Xwk3fAzmzuriIM9_xSG6dmxBw",
-  authDomain: "journal-app-a7a75.firebaseapp.com",
-  projectId: "journal-app-a7a75",
-  storageBucket: "journal-app-a7a75.appspot.com",
-  messagingSenderId: "286742380524",
-  appId: "1:286742380524:web:ba001fde7b7497b4b2cac9",
-  measurementId: "G-2YNK9ZDHWN"
+export const firebaseConfig = {
+  apiKey: "AIzaSyB5a7_cZyqs3r33w_XKMv4UZtyGwCzSK4E",
+  authDomain: "react-app-82896.firebaseapp.com",
+  projectId: "react-app-82896",
+  storageBucket: "react-app-82896.appspot.com",
+  messagingSenderId: "210680262150",
+  appId: "1:210680262150:web:7a93b47ff05e157bea9d87",
+  measurementId: "G-YCBBMCMHR9"
 };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
