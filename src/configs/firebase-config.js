86e5f69a41922ba9bbddfb0969567f5520a8ab21
Yim <yimhernandez1@hotmail.com>
2022-01-15 05:26:13 -0400

    update config

--- src/configs/firebase-config.js
+++ src/configs/firebase-config.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB5a7_cZyqs3r33w_XKMv4UZtyGwCzSK4E",
-  authDomain: 'react-app-82896.firebaseapp.com',
-  projectId: 'react-app-82896',
-  storageBucket:'react-app-82896.appspot.com',
-  messagingSenderId: '210680262150',
-  appId:'1:210680262150:web:7a93b47ff05e157bea9d87',
-  measurementId:'G-YCBBMCMHR9'
+  authDomain: "react-app-82896.firebaseapp.com",
+  projectId: "react-app-82896",
+  storageBucket: "react-app-82896.appspot.com",
+  messagingSenderId: "210680262150",
+  appId: "1:210680262150:web:7a93b47ff05e157bea9d87",
+  measurementId: "G-YCBBMCMHR9",
 };
 
 const app = initializeApp(firebaseConfig);
